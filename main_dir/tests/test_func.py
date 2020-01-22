5c51cb65e0163c469b376d6d371fb80c78a948cb
kbo <khaled.bousrih@netsach.fr>
2020-01-22 11:23:09 +0100

    add test

--- main_dir/tests/test_func.py
+++ main_dir/tests/test_func.py
@@ -6,10 +6,14 @@ class MyFunctionsTestCase(TestCase):
     def test_is_even(self):
         num = 1234
         self.assertTrue(is_even(num))
+        with self.assertRaises(ValueError):
+            is_even('NOT A NUMBER')
 
     def test_is_odd(self):
         num = 12345
         self.assertTrue(is_odd(num))
+        with self.assertRaises(ValueError):
+            is_odd('NOT A NUMBER')
 
     def test_type(self):
         self.assertEqual(odd_even(123), 'ODD')
