1f4bd6cf1ba4f3d4e907fdddab02da5894c5542a
kbo <khaled.bousrih@netsach.fr>
2020-01-23 10:12:03 +0100

    test user

--- main_dir/tests/test_func.py
+++ main_dir/tests/test_func.py
@@ -6,12 +6,14 @@ class MyFunctionsTestCase(TestCase):
     def test_is_even(self):
         num = 1234
         self.assertTrue(is_even(num))
+        self.assertIsNone(is_even(1.2))
         with self.assertRaises(ValueError):
             is_even('NOT A NUMBER')
 
     def test_is_odd(self):
         num = 12345
         self.assertTrue(is_odd(num))
+        self.assertIsNone(is_even(1.3))
         with self.assertRaises(ValueError):
             is_odd('NOT A NUMBER')
 
